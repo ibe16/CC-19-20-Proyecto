da08e1123a037b06061b5ab03e4026e8f685dcf8
ibe16 <irenebejar@correo.ugr.es>
2020-01-06 17:33:29 +0100

    Mock para el servidor SMTP Se ha añadido un mock para el servidor SMTP ya que Travis tiene estos puertos bloqueados issue #46

--- tests/test_notifier_celery.py
+++ tests/test_notifier_celery.py
@@ -1,18 +1,16 @@
 import pytest
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 
 import sys
 sys.path.append('notifier')
 from notifier_celery import app, send_emails
 
-# @pytest.fixture(scope='session')
-# def send_emails():
-#     return Mock()
-
 def setup_module():
     app.conf.update(CELERY_ALWAYS_EAGER=True)
 
 def test_send_email():
-    result = send_emails('1', ['irenebejar@correo.ugr.es'])
-    assert result is None
+    with patch("smtplib.SMTP") as mock_smtp:
+        result = send_emails('1', ['irenebejar@correo.ugr.es'])
+        instance = mock_smtp.return_value
+        assert instance.sendmail.called == True 
     
