b99cc3634502697ec0944bcbb319584e740ade83
ibe16 <irenebejar@correo.ugr.es>
2020-01-17 20:51:26 +0100

    Error de indexación en la docu

--- notifier/NotificationList_model.py
+++ notifier/NotificationList_model.py
@@ -13,15 +13,15 @@ from collections import OrderedDict
 connect(os.environ['MONGO_URI'], alias='notifier')
 
 class NotificationList_model(MongoModel):
-"""Modelo para la base de datos
-    Indicamos que:
-    1) La lista donde se suscribe el email es el ID de nuestro documento y en un String
-    2) Los emails deben tener un formato válido y deben ser únicos para la lista en la que se encuentren"""
+    """Modelo para la base de datos
+        Indicamos que:
+        1) La lista donde se suscribe el email es el ID de nuestro documento y en un String
+        2) Los emails deben tener un formato válido y deben ser únicos para la lista en la que se encuentren"""
     id_line=fields.CharField(primary_key=True)
     email=fields.ListField(field=fields.EmailField(required=True, primary_key=True), required=True)
 
     class Meta:
-    """Indicamos el alias para la BD y la colección donde vamos a almacenar los datos"""
+        """Indicamos el alias para la BD y la colección donde vamos a almacenar los datos"""
         connection_alias='notifier'
         collection_name='notification_list'
         final = True
